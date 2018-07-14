86e229e94dd45bfa3adac113d04f6125de334bc7
purpose50 <purposenigeria@gmail.com>
2018-07-14 21:54:33 +0100

    initial commit

---
+++ webscrapper/main.py
@@ -0,0 +1,45 @@
+from mathematicians import get_names, get_hits_on_name
+
+if __name__ == '__main__':
+    print('Getting the list of names')
+    names = get_names()
+    print('... done.\n')
+
+    results = []
+
+    print('Getting stats for each name....')
+
+    for name in names:
+        try:
+            hits = get_hits_on_name(name)
+            if hits is None:
+                hits = -1
+            results.append((hits, name))
+        except:
+            results.append(-1, name)
+            log_error('error encountered while processing '
+                      '{}, skipping'.format(name))
+
+    print('... done.\n')
+
+    results.sort()
+    results.reverse()
+
+    if len(results) > 5:
+        top_marks = results[:5]
+    else:
+        top_marks = results
+
+    print('\nThe most popular mathematicians are:\n')
+    for (mark, mathematicians) in top_marks:
+        print('{} with {} pageViews'.format(mathematicians, mark))
+
+    no_results = len([res for res in results if res[0] == -1])
+    print('\nBut we did not find results for '
+          '{} mathematicians on the list'.format(no_results))
+
+
+
+
+
+
